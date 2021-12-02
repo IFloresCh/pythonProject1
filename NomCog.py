cbfc139a8779a7733a56f9526d13444fb29834da
IFloresCh <iflores@institutmvm.cat>
2021-12-02 17:00:55 +0100

    :v

--- NomCog.py
+++ NomCog.py
@@ -1,9 +1,22 @@
+# demanar a l'usuari quants registres vol introduir
 def main():
-    name = input("Ingrese Nombre: ")
-    fname = input("Ingrese Primer Apellido: ")
-    lname = input("Ingrese Segundo Apellido: ")
+    lista = []
+    quants = int(input("Ingrese cuantos vas a registrar: "))
+    while quants < 1:
+        quants = int(input("Ingrese cuantos vas a registrar: "))
+    for x in range(quants):
+        name = input("Ingrese Nombre, Apellido1, Apellido2: ")
+        lista.append(name)
+        tel = int(input("Ingrese su numero de telefono: "))
+        while tel < 999999999 or tel > 9999999:
+            tel = int(input("Ingrese su numero de telefono: "))
+        lista.append(tel)
+        edad = int(input("Ingrese su edad: "))
+        lista.append(edad)
+        cont = input("Contacto True or False: ")
+        lista.append(cont)
 
-    print(fname[:2], lname[:2], name[:2])
+    print(lista)
 
 
 if __name__ == '__main__':
