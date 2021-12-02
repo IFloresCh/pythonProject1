c463554bea40fa10ae91f05ce6de54b72aab5920
IFloresCh <iflores@institutmvm.cat>
2021-12-02 15:46:16 +0100

    :v

---
+++ NomCog.py
@@ -0,0 +1,10 @@
+def main():
+    name = input("Ingrese Nombre: ")
+    fname = input("Ingrese Primer Apellido: ")
+    lname = input("Ingrese Segundo Apellido: ")
+
+    print(fname[:2], lname[:2], name[:2])
+
+
+if __name__ == '__main__':
+    main()
