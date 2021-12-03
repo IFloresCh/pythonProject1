ed84d16ca8034339337f2ee8ecb3bf8e11d85986
IFloresCh <iflores@institutmvm.cat>
2021-12-03 03:06:16 +0100

    holi

--- NomCog.py
+++ NomCog.py
@@ -1,20 +1,29 @@
 # demanar a l'usuari quants registres vol introduir
 def main():
     lista = []
-    quants = int(input("Ingrese cuantos vas a registrar: "))
+    quants, tel, edad = 0, 0, 0
+
     while quants < 1:
         quants = int(input("Ingrese cuantos vas a registrar: "))
     for x in range(quants):
         name = input("Ingrese Nombre, Apellido1, Apellido2: ")
         lista.append(name)
-        tel = int(input("Ingrese su numero de telefono: "))
-        while tel < 999999999 or tel > 9999999:
+
+        while tel > 999999999 or tel < 1000000:
             tel = int(input("Ingrese su numero de telefono: "))
         lista.append(tel)
-        edad = int(input("Ingrese su edad: "))
+
+        while edad < 1:
+            edad = int(input("Ingrese su edad: "))
         lista.append(edad)
-        cont = input("Contacto True or False: ")
-        lista.append(cont)
+
+        cont = bool(input("Contacto True or False: "))
+        if cont is True:
+            lista.append(True)
+        elif cont is False:
+            lista.append(False)
+        while cont is not True or cont is not False:
+            cont = input("Contacto True or False: ")
 
     print(lista)
 
