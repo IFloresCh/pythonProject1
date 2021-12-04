b099e47dc60e21d084a100e613f11de258c2b607
IFloresCh <iflores@institutmvm.cat>
2021-12-05 00:14:16 +0100

    its done! ;)

--- NomCog.py
+++ NomCog.py
@@ -1,31 +1,36 @@
 # demanar a l'usuari quants registres vol introduir
 def main():
-    lista = []
-    quants, tel, edad = 0, 0, 0
-
+    nom, cog1, cog2, phone, years, contacto = [], [], [], [], [], []
+    lst = []
+    pr, x, y = 0, 0, 4
+    quants = int(input("Ingrese cuantos vas a registrar: "))
     while quants < 1:
         quants = int(input("Ingrese cuantos vas a registrar: "))
     for x in range(quants):
+
         name = input("Ingrese Nombre, Apellido1, Apellido2: ")
-        lista.append(name)
+        while len(name) > 30:
+            name = input("Ingrese Nombre, Apellido1, Apellido2: ")
+        nom.append(name)
 
-        while tel > 999999999 or tel < 1000000:
+        tel = int(input("Ingrese su numero de telefono: "))
+        while tel > 99999999 or tel < 1000000:
             tel = int(input("Ingrese su numero de telefono: "))
-        lista.append(tel)
+        phone.append(tel)
 
+        edad = int(input("Ingrese su edad: "))
         while edad < 1:
             edad = int(input("Ingrese su edad: "))
-        lista.append(edad)
+        years.append(edad)
 
-        cont = bool(input("Contacto True or False: "))
-        if cont is True:
-            lista.append(True)
-        elif cont is False:
-            lista.append(False)
-        while cont is not True or cont is not False:
-            cont = input("Contacto True or False: ")
+        cont = int(input("Contacto True or False: "))
+        while cont < 0 or cont > 1:
+            cont = int(input("Contacto True or False: "))
+        contacto.append(cont)
 
-    print(lista)
+    print("{:<30} {:<15} {:<10} {:<10}".format('Nombre', 'Telefono', 'Años', 'Contacto'))
+    for x in range(quants):
+        print("{:<30} {:<15} {:<10} {:<10}".format(nom[x], str(phone[x]), str(years[x]), contacto[x]))
 
 
 if __name__ == '__main__':
