b1d07dea7a61967d275266743c370a3bc43ae0ce
IFloresCh <iflores@institutmvm.cat>
2021-12-10 15:00:55 +0100

    Actividad 10/12

---
+++ Practica10_3Con diccionario.py
@@ -0,0 +1,25 @@
+def main():
+    header = ['Username', 'Department', 'Classroom']
+    username = list()
+    department = list()
+    classroom = list()
+    registres = int(input("Cuantos registros quiere introducir: "))
+
+    for i in range(registres):
+        username.append(input("Introduce el usuario: "))
+        department.append(input("Introduce el departamento: "))
+        classroom.append(int(input("Introduce la clase: ")))
+    regs = {
+        'username': username,
+        'department': department,
+        'classroom': classroom
+    }
+    for i in header:
+        print(i, end='\t\t|')
+    print()
+    for i in range(registres):
+        print(regs['username'][i] + '\t\t|' + regs['department'][i] + '\t\t|' + str(regs['classroom'][i]) + '\t\t|')
+
+
+if __name__ == '__main__':
+        main()
